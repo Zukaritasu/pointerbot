f67234315f4063ec7dcd35aee47805345b811ffe
zukaritasu <perezjhonlenno29@gmail.com>
2023-10-30 01:54:45 -0400

    Added isNullOrUndefined function to confirm if a value is null or undefined.

--- src/utils.js
+++ src/utils.js
@@ -36,5 +36,9 @@ module.exports = {
 	 */
 	getTextStyleByPosition(text, position) {
 		return position <= 75 ? `**${text}**` : position > 150 ? text : `*${text}*`;
+	},
+
+	isNullOrUndefined(value) {
+		return value == null || value == undefined;
 	}
 };
