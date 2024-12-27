4581a1cde314c35296c3ff3fe3a517ec0ab7877c
Zukaritasu <perezjhonlenno29@gmail.com>
2024-12-27 16:48:34 +0000

    Updating the code for better performance and security

--- src/commands/level.js
+++ src/commands/level.js
@@ -18,19 +18,106 @@
 const {
 	SlashCommandBuilder,
 	ChatInputCommandInteraction,
-	Client
+	Client,
+	MessageComponentInteraction,
+	StringSelectMenuBuilder,
+	StringSelectMenuOptionBuilder,
+	EmbedBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle
 } = require('discord.js');
 
 const request = require('../request');
-const embed = require('../embeds');
+const embedLevel = require('../embed-level');
+const logger = require('../logger');
+const errorMessages = require('../error-messages');
 const utils = require('../utils');
 const { Db } = require('mongodb');
+const embeds = require('../embeds');
 
 const COUNT_LIST_ELEMENTS = 15;
+const FILTER_TIMEOUT = 120000; // 2 minutes
 
+
+
+
+/**
+ * Generates an embed message with a list of demons and navigation buttons.
+ *
+ * @param {Array} demons - The list of demon objects to display.
+ * @param {number} begin - The starting index of the demons list to display.
+ * @param {number} countDemons - The number of demons to display.
+ * @returns {Object} An object containing the embed message and components.
+ */
+function getLevelListEmbed(demons, begin, countDemons) {
+	let description = '';
+	let countOptions = 0;
+
+	const menu = new StringSelectMenuBuilder()
+	menu.setCustomId('demon')
+	menu.setPlaceholder('Select a demon')
+
+	for (let i = begin; i < demons.length && i < begin + countDemons; i++) {
+		const demon = demons[i];
+		countOptions++;
+		description += `\`${`${(i + 1)}`.padStart(2, '0')}\` - ${demon.name} *by ${demon.publisher.name}*\n`;
+		menu.addOptions(new StringSelectMenuOptionBuilder()
+			.setLabel(`${demon.name} by ${demon.publisher.name}`)
+			.setValue(`${i}`)
+		);
+	}
+
+	const isEmpty = countOptions === 0;
+
+	return {
+		embeds: [
+			new EmbedBuilder()
+				.setColor(embeds.COLOR)
+				.setAuthor(embeds.author)
+				.setTitle('Demons')
+				.setDescription(description)
+				.setTimestamp()
+				.setFooter({ text: `PointerBot` })
+		],
+		components: [
+			new ActionRowBuilder().addComponents(menu),
+			new ActionRowBuilder().addComponents(
+				new ButtonBuilder()
+					.setCustomId('back')
+					.setEmoji('<:retroceder:1320736997941317715>')
+					.setStyle(ButtonStyle.Primary)
+					.setDisabled(isEmpty || begin < countDemons),
+				new ButtonBuilder()
+					.setCustomId('follow')
+					.setEmoji('<:siguiente:1320749783505178725>')
+					.setStyle(ButtonStyle.Primary)
+					.setDisabled(isEmpty || countOptions < countDemons || begin + countDemons >= demons.length),
+				new ButtonBuilder()
+					.setCustomId('close')
+					.setEmoji('<:close:1320737181358227551>')
+					.setStyle(ButtonStyle.Danger)
+			)
+		]
+	}
+}
+
+/**
+ * Fetches demon data from the API based on the provided value.
+ *
+ * @param {number|string} value - The value to query the API with. If a number is provided, it will fetch the demon
+ *                                listed after the given number. If a string is provided, it will search for demons
+ *                                whose names contain the given string.
+ * @returns {Promise<Object>} A promise that resolves to an object containing the error status and the fetched data.
+ *                            If an error occurs, the object will have an `error` property set to true. Otherwise,
+ *                            it will have an `error` property set to false and a `data` property containing the
+ *                            fetched demon data.
+ */
 async function getDemonJSON(value) {
 	let query = (typeof value === 'number') ? `api/v2/demons/listed?limit=1&after=${--value}` :
 		`api/v2/demons/?name_contains=${value.trim().toLowerCase().replace(' ', '+')}`;
+
+	logger.DBG(`Querying Pointercrate API with: ${query}`)
 	const responseData = await request.getResponseJSON(query);
 	if (responseData instanceof Error) {
 		return {
@@ -44,20 +131,31 @@ async function getDemonJSON(value) {
 	}
 }
 
+
 /**
- * @param {ChatInputCommandInteraction} interaction 
- * @param {object} demonJson 
+ * Handles the interaction response message for a level command.
+ * 
+ * @param {ChatInputCommandInteraction} interaction - The interaction object from Discord.
+ * @param {Object} demonJson - The JSON object containing demon data.
+ * @returns {Promise<void>} - A promise that resolves when the interaction is complete.
+ * 
+ * @throws {Error} - Throws an error if there is an issue with the interaction.
  */
 async function waitResponseMessage(interaction, demonJson) {
-	let begin = 0;
-	let response = await interaction.editReply(embed.getLevelListEmbed(demonJson.data, begin, COUNT_LIST_ELEMENTS));
+	let begin = 0,
+		/** @type {MessageComponentInteraction} */
+		confirmation = null,
+		/** @type {{embeds: object[], components: object[]}} */
+		message = null;
+
+	let response = await interaction.editReply(message = getLevelListEmbed(demonJson.data, begin, COUNT_LIST_ELEMENTS));
 	try {
 		while (true) {
 			const collectorFilter = i => i.user.id === interaction.user.id;
-			let confirmation = await response.awaitMessageComponent(
+			confirmation = await response.awaitMessageComponent(
 				{
 					filter: collectorFilter,
-					time: 60000
+					time: FILTER_TIMEOUT
 				}
 			);
 
@@ -65,43 +163,68 @@ async function waitResponseMessage(interaction, demonJson) {
 				begin -= COUNT_LIST_ELEMENTS;
 			} else if (confirmation.customId === 'follow') {
 				begin += COUNT_LIST_ELEMENTS;
+			} else if (confirmation.customId === 'close') {
+				await interaction.deleteReply(); break
 			} else if (confirmation.customId === 'demon') {
-				await confirmation.update(await embed.getDemonEmbed(demonJson.data[parseInt(confirmation.values[0])]))
+				try {
+					await confirmation.update(message = await embedLevel.getDemonEmbed(demonJson.data[parseInt(confirmation.values[0])]))
+					confirmation = await response.awaitMessageComponent(
+						{
+							filter: collectorFilter,
+							time: FILTER_TIMEOUT
+						}
+					);
+
+					if (confirmation.customId === 'close')
+						await interaction.deleteReply();
+				} catch (e) {
+					try {
+						if (e.message !== errorMessages.InteractionCollectorErrorTime) {
+							logger.ERR(e.message);
+							await interaction.editReply('An error has occurred when querying the level')
+						} else {
+							// at 2 is close button
+							message.components.at(0).components.at(2).setDisabled(true);
+							await interaction.editReply({
+								components: message.components,
+								embeds: message.embeds
+							});
+						}
+					} catch (err) {
+						logger.ERR(err.message);
+					}
+				}
 				break;
 			}
-			await confirmation.update(embed.getLevelListEmbed(demonJson.data, begin, COUNT_LIST_ELEMENTS))
+			await confirmation.update(message = getLevelListEmbed(demonJson.data, begin, COUNT_LIST_ELEMENTS))
 		}
 	} catch (e) {
-		console.log(e)
 		try {
-			await interaction.editReply(
-				{
-					content: 'No player has been selected from the drop-down menu',
-					embeds: [],
-					components: []
-				}
-			);
+			if (e.message !== errorMessages.InteractionCollectorErrorTime) {
+				logger.ERR(e.message);
+				await interaction.editReply('An error has occurred when querying the level')
+			} else
+				await interaction.deleteReply();
 		} catch (err) {
-
+			logger.ERR(err.message);
 		}
 	}
 }
 
 /**
- * Creates a request to the Pointercrate API waiting for a response and after
- * having obtained the response it goes through a verification of the data
- * and then calls the waitResponseMessage function.
- * 
- * @param {object} serverInfo 
- * @param {ChatInputCommandInteraction} interaction 
- * @param {string | number} option 
+ * Handles the response message for a level query interaction.
+ *
+ * @param {Object} _serverInfo - The server information (currently unused).
+ * @param {ChatInputCommandInteraction} interaction - The interaction object from the Discord API.
+ * @param {string} option - The level name or position to query.
+ * @returns {Promise<void>} - A promise that resolves when the response message has been handled.
  */
 async function responseMessage(_serverInfo, interaction, option) {
-	let demonJson = await getDemonJSON(option)
+	const demonJson = await getDemonJSON(option)
 	if (demonJson.error || 'message' in demonJson.data /* response error */) {
 		await interaction.editReply('Pointercrate API: an error has occurred when querying the level')
 	} else if (demonJson.data.length === 1) {
-		await utils.responseMessageAwaitClose(interaction, await embed.getDemonEmbed(demonJson.data[0]));
+		await utils.responseMessageAwaitClose(interaction, await embedLevel.getDemonEmbed(demonJson.data[0]));
 	} else if (demonJson.data.length === 0) {
 		await interaction.editReply('Pointercrate API: the name or position of the entered level does not exist')
 	} else {
@@ -110,14 +233,15 @@ async function responseMessage(_serverInfo, interaction, option) {
 }
 
 /**
- * Returns one of the two options entered by the user, which can be either
- * the name of the level or its position in the demonlist. In any case that
- * the user did not enter any value, the function returns null
- * 
- * @param {ChatInputCommandInteraction} interaction 
- * @returns {string | number | null}
+ * Retrieves a user input option from the interaction object.
+ * It first attempts to get a string option with the key 'name'.
+ * If the string option is not provided, it then attempts to get an integer option with the key 'position'.
+ * If the integer option is provided and is less than 0, it defaults to 1.
+ *
+ * @param {Object} interaction - The interaction object containing user input options.
+ * @returns {string|number|null} - The user input option, which can be a string, a non-negative integer, or null if no valid option is provided.
  */
-async function getUserInputOption(interaction) {
+function getUserInputOption(interaction) {
 	let option = interaction.options.getString('name', false);
 	if (utils.isNullOrUndefined(option)) {
 		option = interaction.options.getInteger('position', false)
@@ -129,20 +253,23 @@ async function getUserInputOption(interaction) {
 }
 
 /**
- * @param {Client} _client 
- * @param {Db} database 
- * @param {ChatInputCommandInteraction} interaction 
+ * Executes the level command.
+ *
+ * @param {Client} _client - The client object (not used in this function).
+ * @param {Db} database - The database object for accessing server information.
+ * @param {ChatInputCommandInteraction} interaction - The interaction object representing the command interaction.
+ * @returns {Promise<void>} - A promise that resolves when the command execution is complete.
  */
 async function execute(_client, database, interaction) {
 	await utils.validateServerInfo(interaction, database, false, false, async (serverInfo) => {
-		const option = await getUserInputOption(interaction);
+		const option = getUserInputOption(interaction);
 		if (utils.isNullOrUndefined(option)) {
 			await interaction.editReply(`You have not entered either of the two options. Enter a correct option`);
 		} else {
 			try {
 				await responseMessage(serverInfo, interaction, option)
 			} catch (e) {
-				console.log(e)
+				logger.ERR(e.message);
 			}
 		}
 	})
