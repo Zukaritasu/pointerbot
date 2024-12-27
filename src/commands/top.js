f0088c8470ca5da93d20ba986b3d8a0079605c15
Zukaritasu <perezjhonlenno29@gmail.com>
2024-12-27 16:00:46 +0000

    Updating the code for better performance and security

--- src/commands/top.js
+++ src/commands/top.js
@@ -17,20 +17,52 @@
 
 const { SlashCommandBuilder, ChatInputCommandInteraction } = require('discord.js');
 const request = require('../request');
-const embeds = require('../embeds');
+const embedLevel = require('../embed-level');
 const utils = require('../utils');
+const logger = require('../logger')
+
+//////////////////////////////////////////////////////////////
+
+//----------------------------------------------------------//
+//                         Demonlist                        //
+
+
+/**
+ * Fetches the top demon from the Pointercrate API and sends an embedded message with the demon's details.
+ * If the API returns an empty field, sends a message indicating that.
+ * If an error occurs, logs the error and sends an error message to the interaction.
+ *
+ * @param {ChatInputCommandInteraction} interaction - The interaction object from Discord.js.
+ * @returns {Promise<void>} - A promise that resolves when the interaction reply is sent.
+ */
+async function demonlistTopLevel(interaction) {
+	try {
+		const responseData = await request.getResponseJSON('api/v2/demons/listed?limit=1&after=0')
+		if (!responseData) {
+			await interaction.editReply('Pointercrate API: has returned an empty field')
+			return
+		}
+
+		await utils.responseMessageAwaitClose(interaction, 
+			await embedLevel.getDemonEmbed(responseData.data[0]));
+	} catch (e) {
+		logger.ERR(e)
+		try {
+			await interaction.editReply('An unknown error has occurred')
+		} catch (error) {
+			logger.ERR('Error sending reply')
+		}
+	}
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('top')
-		.setDescription('The top 1 player of the leaderboad'),
+		.setDescription('Demonlist Top 1 Level'),
 
 	async execute(_client, database, interaction) {
 		await utils.validateServerInfo(interaction, database, false, false, async (_serverInfo) => {
-			const responseData = await request.getResponseJSON('api/v2/demons/listed?limit=1&after=0')
-			const message = responseData.data.length === 0 ? 'Pointercrate API: has returned an empty field' : 
-				await embeds.getDemonEmbed(responseData.data[0])
-			await utils.responseMessageAwaitClose(interaction, message)
+			await demonlistTopLevel(interaction)
 		})
 	}
 };
